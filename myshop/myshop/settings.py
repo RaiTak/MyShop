e83f29937217533ff5a3303826df283a66c1e8c7
RaiTak <kairat_raitak@mail.ru>
2024-05-05 12:11:09 +0500

    0.0.3 Add Postgresql

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myshopdb',
+        'USER': 'myshop',
+        'PASSWORD': 'myshopmyshop',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
