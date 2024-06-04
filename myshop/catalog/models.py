da09a3d5d1ba7aed8b32ff4607c21f83e101dba3
RaiTak <kairat_raitak@mail.ru>
2024-06-04 16:29:16 +0500

    Create Docke Compose and models Book, Author

--- myshop/catalog/models.py
+++ myshop/catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name='Имя', help_text='Введите имя автора')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия', help_text='Введите фамилию автора')
+    slug = models.SlugField(max_length=250, unique=True, verbose_name='URL-адрес')
+    photo = models.ImageField(upload_to='author_photos/', blank=True, null=True, verbose_name='Фотография')
+    biography = models.TextField(verbose_name='Биография', help_text='Краткая биография автора', blank=True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Название', help_text="Введите название книги")
+    isbn = models.CharField(max_length=17, verbose_name='ISBN')
+    cover = models.ImageField(upload_to='cover_images/', blank=True, verbose_name='Обложка')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    year = models.PositiveIntegerField(verbose_name='Год издания')
+    language = models.CharField(max_length=100, blank=True, verbose_name='Язык')
+    publisher = models.CharField(max_length=200, blank=True, null=True, verbose_name='Издательство')
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name='Цена')
+    is_available = models.BooleanField(default=True, verbose_name='В наличии')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1=v(ta52l93wr-nc$+z%dyn*#g!lyna-4#7s2ft%)e6l&^^4ax'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', '0')))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(",")
 
 
 # Application definition
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myshopdb',
-        'USER': 'myshop',
-        'PASSWORD': 'myshopmyshop',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        'NAME': os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        'USER': os.environ.get("SQL_USER", "user"),
+        'PASSWORD': os.environ.get("SQL_PASSWORD", "password"),
+        'HOST': os.environ.get("SQL_HOST", "localhost"),
+        'PORT': os.environ.get("SQL_PORT", "5432"),
     }
 }
 
